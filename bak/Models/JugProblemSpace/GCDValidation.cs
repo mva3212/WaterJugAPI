53dd9ee6f2e288ad2e96978aad2196c680e560e7
mva3212 <michael.v.alt@gmail.com>
2014-10-27 14:33:07 -0700

    inital commit

---
+++ bak/Models/JugProblemSpace/GCDValidation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WaterJugAPI.Models.JugProblemSpace
+{
+    public class GCDValidation
+    {
+    }
+}
